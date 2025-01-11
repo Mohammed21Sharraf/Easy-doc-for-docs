7827892d8bfe86a8a41a468e6a9016d7a47cb044
KaziQuader <zunayedquader@gmail.com>
2025-01-11 16:35:01 +0600

    Scaled the numerical columns

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from pathlib import Path
 from utilities.preprocess import preprocess_blood_test_report, add_patient_id_col
+from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 if __name__ == "__main__":
     blood_test_report_df = pd.read_csv('raw data/Blood Test Report.csv')
@@ -11,5 +13,8 @@ if __name__ == "__main__":
     filepath.parent.mkdir(parents=True, exist_ok=True)  
     blood_test_report_preprocessed.to_csv(filepath)  
 
-    
+    numeric_cols = blood_test_report_preprocessed.select_dtypes(include=np.number).columns.to_list()
+    scaler = StandardScaler()
+    blood_test_report_preprocessed[numeric_cols] = scaler.fit_transform(blood_test_report_preprocessed[numeric_cols])
 
+    
